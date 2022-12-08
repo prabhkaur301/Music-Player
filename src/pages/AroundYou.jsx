4e76fdebe9130160136a4fd20b4c4bab91bb7ebc
prabhkaur301 <pk746467@gmail.com>
2022-12-08 21:45:41 +0530

    env variables bug fix

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -9,16 +9,16 @@ const AroundYou = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${process.env.VITE_GEO_API_KEY}`)
+        axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
             .then((res) => setLocation(res.data.location))
             .catch((error) => console.log(error))
             .finally(setLoading(false))
 
-    }, [location])
+    }, [])
     
     const { data, isFetching, error } = useGetSongsByCountryQuery(location.country || 'IN');
     const { activeSong, isPlaying } = useSelector((state) => state.player);
-    console.log(data)
+    // console.log(data)
     if (isFetching && loading)
         return <Loader title="Loading songs around your location..." />
 
@@ -28,7 +28,7 @@ const AroundYou = () => {
     return (
         <div className="flex flex-col">
             <h2 className="text-white text-3xl font-bold mt-4 mb-10">Around You  
-            <span className="text-white text-3xl font-bold mt-4 mb-10"> ({location?.region || 'India'})</span> </h2>
+            <span className="text-white text-3xl font-bold mt-4 mb-10"> ({'India'})</span> </h2>
             <div className='flex flex-wrap gap-8 sm:justify-start justify-center'>
                 {data?.map((song, idx) => {
                     return (
